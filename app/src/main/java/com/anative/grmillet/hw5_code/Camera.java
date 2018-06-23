a35d6d6329b1ef8f1ab9e8e7f6a7d1f2fb0edd54
강현구 <brusque4owl@gmail.com>
2018-06-24 02:46:48 +0900

    180624_0246: complete camera branch

--- app/src/main/java/com/anative/grmillet/hw5_code/Camera.java
+++ app/src/main/java/com/anative/grmillet/hw5_code/Camera.java
@@ -6,7 +6,6 @@ package com.anative.grmillet.hw5_code;
 
 public class Camera {
 
-
     private float[] pos = new float[3]; // camera position
     private float[] uAxis = new float[3], vAxis = new float[3],
             nAxis = new float[3];
@@ -16,21 +15,16 @@ public class Camera {
     private final float DELTA_CONSTANT = 0.01f;
     private final float TO_RADIAN = 0.01745329f; // pi/180
 
+    private final float INIT_POS_X = 5.0f;
+    private final float INIT_POS_Y = 10.0f;
+    private final float INIT_POS_Z = 20.0f;
+
     // //////////////////////////////////////////////////////////////////////
     public Camera() { // initial Value
-        pos[0] = 0.0f;
-        pos[1] = 2.0f;
-        pos[2] = 10.0f;
-        uAxis[0] = 1.0f;
-        uAxis[1] = 0.0f;
-        uAxis[2] = 0.0f;
-        vAxis[0] = 0.0f;
-        vAxis[1] = 1.0f;
-        vAxis[2] = 0.0f;
-        nAxis[0] = 0.0f;
-        nAxis[1] = 0.0f;
-        nAxis[2] = 1.0f;
-
+        pos[0] = INIT_POS_X;        pos[1] = INIT_POS_Y;        pos[2] = INIT_POS_Z;
+        uAxis[0] = 1.0f;        uAxis[1] = 0.0f;        uAxis[2] = 0.0f;
+        vAxis[0] = 0.0f;        vAxis[1] = 1.0f;        vAxis[2] = 0.0f;
+        nAxis[0] = 0.0f;        nAxis[1] = 0.0f;        nAxis[2] = 1.0f;
         fovy = 60.0f;
     }
 
@@ -38,8 +32,8 @@ public class Camera {
         float rad_angle, sina, cosa;
 
         rad_angle = TO_RADIAN * dPitch * DELTA_CONSTANT * 10.0f;
-        sina = (float)Math.sin(rad_angle);
-        cosa = (float)Math.cos(rad_angle);
+        sina = (float)Math.sin(-rad_angle);
+        cosa = (float)Math.cos(-rad_angle);
         // calculate v vector
         vAxis[0] = cosa * vAxis[0] + sina
                 * (uAxis[1] * vAxis[2] - uAxis[2] * vAxis[1]);
@@ -59,23 +53,68 @@ public class Camera {
     }
 
     public void Yaw(float dYaw) { // rotate v , n->u
+        float rad_angle, sina, cosa;
+
+        rad_angle = TO_RADIAN * dYaw * DELTA_CONSTANT * 10.0f;
+        sina = (float)Math.sin(-rad_angle);
+        cosa = (float)Math.cos(-rad_angle);
+        // calculate n vector
+        nAxis[0] = cosa * nAxis[0] + sina
+                * (vAxis[1] * nAxis[2] - vAxis[2] * nAxis[1]);
+        nAxis[1] = cosa * nAxis[1] + sina
+                * (-vAxis[0] * nAxis[2] + vAxis[2] * nAxis[0]);
+        nAxis[2] = cosa * nAxis[2] + sina
+                * (vAxis[0] * nAxis[1] - vAxis[1] * nAxis[0]);
+
+        uAxis[0] = vAxis[1] * nAxis[2] - vAxis[2] * nAxis[1];
+        uAxis[1] = -vAxis[0] * nAxis[2] + vAxis[2] * nAxis[0];
+        uAxis[2] = vAxis[0] * nAxis[1] - vAxis[1] * nAxis[0];
+
+        nomalize(vAxis);
+        nomalize(uAxis);
+        nomalize(nAxis);
     }
 
     public void Roll(float dRoll) { // rotate n , u->v
+        float rad_angle, sina, cosa;
+
+        rad_angle = TO_RADIAN * dRoll * DELTA_CONSTANT * 10.0f;
+        sina = (float)Math.sin(-rad_angle);
+        cosa = (float)Math.cos(-rad_angle);
+        // calculate u vector
+
+        uAxis[0] = cosa * uAxis[0] + sina
+                * (nAxis[1] * uAxis[2] - nAxis[2] * uAxis[1]);
+        uAxis[1] = cosa * uAxis[1] + sina
+                * (-nAxis[0] * uAxis[2] + nAxis[2] * uAxis[0]);
+        uAxis[2] = cosa * uAxis[2] + sina
+                * (nAxis[0] * uAxis[1] - nAxis[1] * uAxis[0]);
+
+        vAxis[0] = nAxis[1] * uAxis[2] - nAxis[2] * uAxis[1];
+        vAxis[1] = -nAxis[0] * uAxis[2] + nAxis[2] * uAxis[0];
+        vAxis[2] = nAxis[0] * uAxis[1] - nAxis[1] * uAxis[0];
+
+        nomalize(vAxis);
+        nomalize(uAxis);
+        nomalize(nAxis);
     }
 
     public void MoveForward(float delta) {
-
+        pos[0] += delta * nAxis[0] * DELTA_CONSTANT;
+        pos[1] += delta * nAxis[1] * DELTA_CONSTANT;
+        pos[2] += delta * nAxis[2] * DELTA_CONSTANT;
     }
 
     public void MoveSideward(float delta) {
-        pos[0] += delta * uAxis[0] * DELTA_CONSTANT;
-        pos[1] += delta * uAxis[1] * DELTA_CONSTANT;
-        pos[2] += delta * uAxis[2] * DELTA_CONSTANT;
+        pos[0] -= delta * uAxis[0] * DELTA_CONSTANT;
+        pos[1] -= delta * uAxis[1] * DELTA_CONSTANT;
+        pos[2] -= delta * uAxis[2] * DELTA_CONSTANT;
     }
 
     public void MoveUpward(float delta) {
-
+        pos[0] -= delta * vAxis[0] * DELTA_CONSTANT;
+        pos[1] -= delta * vAxis[1] * DELTA_CONSTANT;
+        pos[2] -= delta * vAxis[2] * DELTA_CONSTANT;
     }
 
     public float[] GetPosition() {
@@ -111,7 +150,11 @@ public class Camera {
     }
 
     public void Zoom(float delta) {
-
+        fovy += delta*0.1f;
+        if(fovy>=180.0f)
+            fovy=179.9f;
+        if(fovy<=0.0f)
+            fovy=0.1f;
     }
 
     public void nomalize(float[] vec) {
